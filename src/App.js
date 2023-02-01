dd40b5748a6b8bafb2adb45f10a499d9cfe82b8b
ManoaRRR <hei.manoa.2@gmail.com>
2023-02-01 07:43:30 +0100

    upload files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoForms from './components/TodoForms';
+
+
+function App() {
+  return (
+      <TodoForms/>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForms.js
@@ -0,0 +1,65 @@
+import React,{useState} from 'react';
+function TodoForms (){
+    const [tache, setTaches]= useState([]);
+    const [newTaches , setNewTaches]= useState("");
+    const [checkTask, setcheckTast]= useState([]);     
+     
+    function handleSubmit (e){
+        e.preventDefault();
+        setTaches([...tache, newTaches]);
+        setNewTaches("");
+     }
+     function handleChange (e){
+        setNewTaches(e.target.value);
+     }
+     function handleCheck(e){
+        if(e.target){
+            setcheckTast([...checkTask, e.target.value]);
+        } else{
+            setcheckTast(
+                checkTask.filter((task) => task !== e.target.value)
+            );
+        }
+     }
+     return (
+        <div >
+          <div >
+          <form onSubmit={handleSubmit}>
+          <h1>to do:</h1>
+            <input
+              type="text"
+              placeholder="Nouvelle tâche"
+              value={newTaches}
+              onChange={handleChange}
+            />
+            <button type="submit">Ajouter</button>
+          </form>
+         
+          <div>
+            {tache.map((task, index) => (
+              <div key={index}>
+                <input
+                  type="checkbox"
+                  id={task}
+                  name={task}
+                  value={task}
+                  onChange={handleCheck}
+                />
+                <label for={task}>{task}</label>
+              </div>
+            ))}
+          </div>
+          </div>
+          <div >
+            <h1>Done: </h1>
+            <ul>
+              {checkTask.map((task, index) => (
+                <li key={index}>{task}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+
+}
+export default TodoForms;
